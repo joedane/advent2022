6234f4ad68f78ed02f803ddac0ab5604586d7d5b
Joe Dane <joe.dane@gmail.com>
2023-06-08 20:06:26 -1000

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,49 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::io::{self, prelude::*};
+
+pub(crate) trait Puzzle {
+    fn solve<R: BufRead, W: Write>(&self, scan: &mut Scanner<R>, w: &mut W) -> io::Result<()>;
+}
+
+#[allow(dead_code)]
+pub(crate) fn run<P: Puzzle>(puzzle: P) -> io::Result<()> {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = Scanner::new(io::BufReader::new(stdin.lock()));
+    let mut out = io::BufWriter::new(stdout.lock());
+    puzzle.solve(&mut scan, &mut out)
+}
+struct Scanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: std::str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: BufRead> Scanner<R> {
+    fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    fn token<T: std::str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = std::str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 }
